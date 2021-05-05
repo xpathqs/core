a2cba47935c57af90db464619be43ae57c59a620
Nikita <gggodhand@gmail.com>
2021-05-05 14:32:34 +0300

    closes #15
    * closes #15
    
    * fix of jacoco 1
    
    * fix of jacoco 2
    
    * fix of jacoco 3
    
    * fix of jacoco 4
    
    * fix of jacoco 5
    
    * fix of jacoco 6
    
    * fix of jacoco 7
    
    * upload badge
    
    Co-authored-by: nikitachegodaev <n.chegodaev@orbita.center>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    `java-library`
-}
-
 group = "org.nachg.xpathqs"
 version = "0.0.1"
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    `java-library`
+    jacoco
+}
 
 repositories {
     mavenCentral()
@@ -38,5 +39,12 @@ tasks.jar {
                 "Implementation-Version" to project.version
             )
         )
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = true
     }
 }
