2b9e5afc971d6aa895b00d59f0b5e9c3d29834b8
Nikita <gggodhand@gmail.com>
2021-10-06 12:35:35 +0300

    closes #143

--- build.gradle.kts
+++ build.gradle.kts
@@ -195,6 +195,6 @@ pitest {
     outputFormats.set(arrayListOf("HTML"))
     jvmArgs.set(arrayListOf("-Xmx1024m"))
 
-    mutationThreshold.set(90)
+    mutationThreshold.set(85)
     coverageThreshold.set(95)
 }
